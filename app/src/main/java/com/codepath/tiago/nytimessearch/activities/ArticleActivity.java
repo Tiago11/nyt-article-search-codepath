9d139d1b00914ce967f5a9d81d038894e21a8dec
tiago <paezsantiago@gmail.com>
2017-09-21 21:46:57 -0300

    Improve the embedded WebView browser.

--- app/src/main/java/com/codepath/tiago/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/codepath/tiago/nytimessearch/activities/ArticleActivity.java
@@ -1,12 +1,16 @@
 package com.codepath.tiago.nytimessearch.activities;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.codepath.tiago.nytimessearch.models.Article;
 import com.codepath.tiago.nytimessearch.R;
+import com.codepath.tiago.nytimessearch.models.Article;
 
 public class ArticleActivity extends AppCompatActivity {
 
@@ -20,18 +24,36 @@ public class ArticleActivity extends AppCompatActivity {
 
         Article article = (Article) getIntent().getSerializableExtra("article");
 
+        // Get a reference to the WebView.
         WebView webView = (WebView) findViewById(R.id.wvArticle);
+        // Configure related browser settings.
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        // Configure the client to use when opening URLs.
+        webView.setWebViewClient(new MyArticleBrowser());
 
-        webView.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
-
-
+        // Load the URL.
         webView.loadUrl(article.getWebUrl());
 
+    }
+
+    // Manages the behavior when URLs are loaded.
+    private class MyArticleBrowser extends WebViewClient {
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
     }
 }
