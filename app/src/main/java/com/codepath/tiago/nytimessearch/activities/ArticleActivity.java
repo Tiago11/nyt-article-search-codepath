3c7934662356174eae7629e50055079ae5c90eb0
tiago <paezsantiago@gmail.com>
2017-09-23 20:16:22 -0300

    Add share action to ArticleActivity's ActionBar.

--- app/src/main/java/com/codepath/tiago/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/codepath/tiago/nytimessearch/activities/ArticleActivity.java
@@ -1,9 +1,14 @@
 package com.codepath.tiago.nytimessearch.activities;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -18,6 +23,8 @@ public class ArticleActivity extends AppCompatActivity {
 
     // Tag for logging.
     private final String TAG = ArticleActivity.class.toString();
+
+    WebView wvArticle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +41,42 @@ public class ArticleActivity extends AppCompatActivity {
         setupWebView(article);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+        // Locate MenuItem with ShareActionProvider.
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider.
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        shareActionProvider.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     /*
      * Sets the necessary properties to show the article in an embedded WebView.
      * Loads the article into the WebView.
      */
     private void setupWebView(Article article) {
         // Get a reference to the WebView.
-        WebView webView = (WebView) findViewById(R.id.wvArticle);
+        wvArticle = (WebView) findViewById(R.id.wvArticle);
         // Configure related browser settings.
-        webView.getSettings().setLoadsImagesAutomatically(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
+        wvArticle.getSettings().setLoadsImagesAutomatically(true);
+        wvArticle.getSettings().setJavaScriptEnabled(true);
+        wvArticle.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wvArticle.getSettings().setUseWideViewPort(true);
+        wvArticle.getSettings().setLoadWithOverviewMode(true);
         // Configure the client to use when opening URLs.
-        webView.setWebViewClient(new MyArticleBrowser());
+        wvArticle.setWebViewClient(new MyArticleBrowser());
 
         // Load the URL.
-        webView.loadUrl(article.getWebUrl());
+        wvArticle.loadUrl(article.getWebUrl());
     }
 
     // Manages the behavior when URLs are loaded.
